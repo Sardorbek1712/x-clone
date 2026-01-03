8d3499af4d7451b9e546f0533feb5dccd00c0b17
Sardorbek1712 <sardor.tolibov04@gmail.com>
2026-01-03 11:03:44 +0100

    Second day

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -4,6 +4,11 @@ import React from "react";
 export default function Page() {
   const user = false;
 
-  if (!user) return <Auth />;
+  if (!user)
+    return (
+      <div className="container h-screen mx-auto max-w-7xl">
+        <Auth />
+      </div>
+    );
   return <div>Page</div>;
 }
--- components/auth/index.tsx
+++ components/auth/index.tsx
@@ -1,41 +1,71 @@
 import Button from "@/ui/button";
 import Image from "next/image";
 import React from "react";
+import { AiFillGithub } from "react-icons/ai";
+import { BsGithub } from "react-icons/bs";
+import { FaGithub } from "react-icons/fa";
 import { FcGoogle } from "react-icons/fc";
+import { GrGithub } from "react-icons/gr";
 
 export default function Auth() {
   return (
     <>
-      <div className="grid grid-cols-2 gap-10 items-center h-screen">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-10 items-center h-screen">
         <Image
           src={"/images/x.svg"}
           alt="X"
           width={450}
           height={450}
-          className="justify-self-end"
+          className="justify-self-center hidden md:block"
         />
 
-        <div className="flex flex-col justify-between h-[70vh]">
+        <div className="flex flex-col justify-center md:justify-between gap-6 h-full md:h-[70vh]">
+          <div className="block md:hidden">
+            <Image src={"/images/x.svg"} alt="X" width={50} height={50} />
+          </div>
           <h1 className="text-6xl font-bold">Happening now</h1>
-          <div className="w-[60%]">
+          <div className="w-full md:w-[60%]">
             <h2 className="text-bold text-3xl mb-4">Joing today.</h2>
             <div className="flex flex-col space-y-2">
-              <Button label={
-                <div className="flex gap-2 items-center justify-center">
-                  <FcGoogle />
-                  Sign up with Google
-                </div>
-              } fullwidth secondary />
-              <Button label="Sign up with Github" fullwidth secondary />
-              <p>or</p>
-              <button>Create account</button>
+              <Button
+                label={
+                  <div className="flex gap-2 items-center justify-center">
+                    <FcGoogle />
+                    Sign up with Google
+                  </div>
+                }
+                fullwidth
+                secondary
+              />
+              <Button
+                label={
+                  <div className="flex gap-2 items-center justify-center">
+                    <AiFillGithub />
+                    Sign up with Github
+                  </div>
+                }
+                fullwidth
+                secondary
+              />
+              <div className="flex items-center justify-center">
+                <div className="h-px bg-gray-700 w-1/2" />
+                <p className="mx-4">or</p>
+                <div className="h-px bg-gray-700 w-1/2" />
+              </div>
+              <Button label={"Create an account"} fullwidth />
+              <div className="text-[10px] text-gray-400">
+                By signing up, you agree to the
+                <span className="text-sky-500"> Terms of Service</span> and
+                <span className="text-sky-500"> Privacy Policy</span>, including
+                <span className="text-sky-500"> Cookie Use</span>.
+              </div>
             </div>
           </div>
-          <div className="w-[60%]">
+          <div className="w-full md:w-[60%]">
             <h3 className="font-medium text-xl mb-4">
               Already have an account?
             </h3>
-            <button>Sign in</button>
+            <Button label={"Sign in"} fullwidth outline />
           </div>
         </div>
       </div>
